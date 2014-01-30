d807982bda16eacaba86d4e80af42e1cf5177455
Chris Shattuck <ashattuc@gmail.com>
2014-01-30 16:10:17 -0700

    After step 2, part 3 - Added examples of using request and response in comments

--- index.php
+++ index.php
@@ -4,11 +4,74 @@ require_once __DIR__.'/autoload.php';
  
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
- 
+
 $request = Request::createFromGlobals();
+
+// You can also simulate a request:
+// $request = Request::create('/index.php?name=Fabien');
+
+
+// Examples of what we can do with $request:
+
+// the URI being requested (e.g. /about) minus any query parameters
+//$request->getPathInfo();
+ 
+// retrieve GET and POST variables respectively
+//$request->query->get('foo');
+//$request->request->get('bar', 'default value if bar does not exist');
+ 
+// retrieve SERVER variables
+//$request->server->get('HTTP_HOST');
+ 
+// retrieves an instance of UploadedFile identified by foo
+//$request->files->get('foo');
+ 
+// retrieve a COOKIE value
+//$request->cookies->get('PHPSESSID');
+ 
+// retrieve an HTTP request header, with normalized, lowercase keys
+//$request->headers->get('host');
+//$request->headers->get('content_type');
+ 
+//$request->getMethod();    // GET, POST, PUT, DELETE, HEAD
+//$request->getLanguages(); // an array of languages the client accepts
+
+
+/*
+// Example with getting a client IP address:
+
+// Doesn't work with a reverse proxy.
+if ($myIp == $_SERVER['REMOTE_ADDR']) {
+    // the client is a known one, so give it some more privilege
+}
+// Works with reverse proxy.
+if ($myIp == $_SERVER['HTTP_X_FORWARDED_FOR'] || $myIp == $_SERVER['REMOTE_ADDR']) {
+    // the client is a known one, so give it some more privilege
+}
+// Works with chained proxies
+$request = Request::createFromGlobals();
+if ($myIp == $request->getClientIp()) {
+    // the client is a known one, so give it some more privilege
+}
+*/
+
+
  
 $input = $request->get('name', 'World');
  
 $response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+
+// Examples what what we can do with $response:
+//$response = new Response();
  
-$response->send();
+//$response->setContent('Hello world!');
+//$response->setStatusCode(200);
+//$response->headers->set('Content-Type', 'text/html');
+ 
+// configure the HTTP cache headers
+//$response->setMaxAge(10);
+
+
+ 
+$response->send(); 
