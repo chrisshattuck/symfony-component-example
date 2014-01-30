1671e346bc575a72ea2f83f7a02d3ed1a8abc185
Chris Shattuck <ashattuc@gmail.com>
2014-01-30 16:12:51 -0700

    After step 3, part 1 - Removing commented examples, added second page, bye.php

--- bye.php
+++ bye.php
@@ -1,5 +1,7 @@
 <?php
  
+// framework/bye.php
+ 
 require_once __DIR__.'/autoload.php';
  
 use Symfony\Component\HttpFoundation\Request;
@@ -7,8 +9,5 @@ use Symfony\Component\HttpFoundation\Response;
  
 $request = Request::createFromGlobals();
  
-$input = $request->get('name', 'World');
- 
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
- 
+$response = new Response('Goodbye!');
 $response->send();
