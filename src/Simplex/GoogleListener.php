bbcbcb71444befb0ab8508e819647dca725b1229
Chris Shattuck <ashattuc@gmail.com>
2014-01-31 15:30:27 -0700

    After step 9, part 4 - Used subscribers instead of listeners
    Using subscribers means that the listener classes now have control over
    defining what they listen to.

--- src/Simplex/GoogleListener.php
+++ src/Simplex/GoogleListener.php
@@ -1,9 +1,15 @@
 <?php
  
 namespace Simplex;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  
-class GoogleListener
+class GoogleListener implements EventSubscriberInterface
 {
+    public static function getSubscribedEvents()
+    {
+        return array('response' => 'onResponse');
+    }
     public function onResponse(ResponseEvent $event)
     {
         $response = $event->getResponse();
