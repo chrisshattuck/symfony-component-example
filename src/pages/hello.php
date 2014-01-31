76744c0029dcb8250d386655f390a12d75f068dc
Chris Shattuck <ashattuc@gmail.com>
2014-01-31 10:58:24 -0700

    After step 3, part 5 - Converting pages over to templates

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,6 +1,3 @@
-<?php
+<?php $name = $request->get('name', 'World') ?>
  
-// framework/hello.php
- 
-$input = $request->get('name', 'World');
-$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
