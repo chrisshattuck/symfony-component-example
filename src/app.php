4efcd620abd5b743e288dee90b4f9fb514ed7396
Chris Shattuck <ashattuc@gmail.com>
2014-01-31 12:46:29 -0700

    After step 5, part 2 - Replacing our app with a new app to calculate when leap years occur.

--- src/app.php
+++ src/app.php
@@ -1,38 +1,26 @@
 <?php
  
 use Symfony\Component\Routing;
+use Symfony\Component\HttpFoundation\Response;
+ 
+function is_leap_year($year = null) {
+    if (null === $year) {
+        $year = date('Y');
+    }
+ 
+    return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
  
 $routes = new Routing\RouteCollection();
-/*
-// Example of adding a function name for the controller.
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
-    'name' => 'World',
-    '_controller' => 'render_template',
-)));
-*/
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
-    'name' => 'World',
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
     '_controller' => function ($request) {
-        return render_template($request);
+        if (is_leap_year($request->attributes->get('year'))) {
+            return new Response('Yep, this is a leap year!');
+        }
+ 
+        return new Response('Nope, this is not a leap year.');
     }
 )));
-/*
-// Example of setting attributes and headers in an anonymous controller function.
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
-    'name' => 'World',
-    '_controller' => function ($request) {
-        // $foo will be available in the template
-        $request->attributes->set('foo', 'bar');
- 
-        $response = render_template($request);
- 
-        // change some header
-        $response->headers->set('Content-Type', 'text/plain');
- 
-        return $response;
-    }
-)));
-*/
-$routes->add('bye', new Routing\Route('/bye', array('_controller' => 'render_template')));
  
 return $routes;
