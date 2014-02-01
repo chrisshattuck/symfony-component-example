0e18af6b6562677c4189a91c898b2361c5c8193e
Chris Shattuck <ashattuc@gmail.com>
2014-01-31 21:23:40 -0700

    After step 11, part 1 - Switching framework to use HttpKernal
    The whole series has basically been leading up to the point of what the
    HttpKernal is all about. In this part, on
    http://fabien.potencier.org/article/60/create-your-own-framework-on-top-
    of-the-symfony2-components-part-11, we can remove nearly all of our
    code from our Framework class and use HttpKernal to do the heavy
    lifting.

--- front.php
+++ front.php
@@ -9,23 +9,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\HttpCache\HttpCache;
-use Symfony\Component\HttpKernel\HttpCache\Store;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/src/app.php';
  
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 $resolver = new HttpKernel\Controller\ControllerResolver();
 
 $dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-$dispatcher->addSubscriber(new Simplex\GoogleListener());
+$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
  
-$framework = new Simplex\Framework($dispatcher, $matcher, $resolver);
-$framework = new HttpCache($framework, new Store(__DIR__.'/cache'));
+$framework = new Simplex\Framework($dispatcher, $resolver);
+ 
 $response = $framework->handle($request);
- 
 $response->send();
